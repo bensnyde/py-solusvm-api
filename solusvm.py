277d3d81c8531759d436e5ee3f2b17999f9aa611
Benton Snyder <benton.snyder@gmail.com>
2013-08-15 18:20:00 -0500

    Update solusvm.py

--- solusvm.py
+++ solusvm.py
@@ -4,6 +4,7 @@
 =====================================================
 :Info: See <http://docs.solusvm.com/v2/Default.htm#Developer/Admin-Api/Admin-Api.htm> for  API implementation.
 :Author: Benton Snyder <introspectr3@gmail.com>
+:Website: Noumenal Designs <http://www.noumenaldesigns.com>
 :Date: $Date: 2013-08-15 22:27:40 -0600 (Thurs, 15 Aug 2013) $
 :Revision: $Revision: 0021 $
 :Description: Python library for interfacing with SolusVM <http://www.solusvm.com>
