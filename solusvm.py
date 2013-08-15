b0e50f83fd954b0f4a3c9fb331c1a3749bdafc8d
Benton Snyder <benton.snyder@gmail.com>
2013-08-15 17:44:22 -0500

    Update solusvm.py

--- solusvm.py
+++ solusvm.py
@@ -3,7 +3,7 @@
  SolusVM XMLRPC API Python Library
 =====================================================
 :Info: See <http://docs.solusvm.com/v2/Default.htm#Developer/Admin-Api/Admin-Api.htm> for  API implementation.
-:Author: Benton Snyder <goodger@python.org>
+:Author: Benton Snyder <introspectr3@gmail.com>
 :Date: $Date: 2013-08-15 22:27:40 -0600 (Thurs, 15 Aug 2013) $
 :Revision: $Revision: 0021 $
 :Description: Python library for interfacing with SolusVM <http://ww.solusvm.com>
