bcc7cebf6834fc79759067a696a2b894aa3fc325
Benton Snyder <benton.snyder@gmail.com>
2013-08-15 17:52:06 -0500

    Update solusvm.py

--- solusvm.py
+++ solusvm.py
@@ -6,7 +6,7 @@
 :Author: Benton Snyder <introspectr3@gmail.com>
 :Date: $Date: 2013-08-15 22:27:40 -0600 (Thurs, 15 Aug 2013) $
 :Revision: $Revision: 0021 $
-:Description: Python library for interfacing with SolusVM <http://ww.solusvm.com>
+:Description: Python library for interfacing with SolusVM <http://www.solusvm.com>
 """
 import requests
 
